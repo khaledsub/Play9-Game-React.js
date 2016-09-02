9ae52fc0f1f65ee5cc5d900bcbba52c687f445cf
Khaled Alsubaihin <kalsubaihin@delegata.com>
2016-09-01 18:04:15 -0700

    added answer checking and accepting, as well some other small pieces, just diff it if you really wanna know

--- script.jsx
+++ script.jsx
@@ -22,9 +22,42 @@ var ButtonFrame = React.createClass( {
   render: function() {
     var checkNum = this.props.checkNum;
     var result = this.props.result;
+    var disabled = (this.props.selectedNums.length === 0);
+    var correct = (this.props.correct);
+    var checkAnswer = this.props.checkAnswer;
+    var acceptAnswer = this.props.acceptAnswer;
+    var button;
+    
+    switch(correct) {
+      case true:
+        button = (
+          <button className="btn btn-success btn-lg"
+                  onClick={acceptAnswer}>
+            <span className="glyphicon glyphicon-ok"></span>
+          </button>
+        );
+        break;
+      case false:
+        button = (
+          <button className="btn btn-danger btn-lg">
+            <span className="glyphicon glyphicon-remove"></span>
+          </button>
+        );
+        break;
+      default:
+        button = (
+          <button className="btn btn-primary btn-lg" 
+                  disabled={disabled}
+                  onClick={checkAnswer}>
+          =
+          </button>
+        )
+      
+    }
+    
     return (
       <div id="button-frame">
-        <button className="btn btn-primary btn-lg">=</button>
+        {button}
       </div>    
     )
   }
@@ -56,10 +89,12 @@ var NumbersFrame = React.createClass( {
     
     var numbers = [], className, 
     clickNum = this.props.clickNum,
-    selectedNums=this.props.selectedNums;
+    selectedNums=this.props.selectedNums,
+    usedNums = this.props.usedNums;
     
     for(var i = 1; i < 10; i++ ){
       className = "number selected-" + (selectedNums.indexOf(i)>=0);
+      className += " used-" + (usedNums.indexOf(i)>=0);
       numbers.push(<div className={className} onClick={clickNum.bind(null, i)}>
                     {i}
                    </div>);
@@ -80,14 +115,17 @@ var Game = React.createClass({
   getInitialState: function() {
     return( {numStars: Math.floor(Math.random()*9) + 1,
              selectedNums: [],
-             result: "="
+             usedNums: [],
+             correct: null
     } );
   },
   
   clickNum: function(clickedNum) {
     if(this.state.selectedNums.indexOf(clickedNum) < 0) {
       this.setState(
-          {selectedNums: this.state.selectedNums.concat(clickedNum)}
+          {selectedNums: this.state.selectedNums.concat(clickedNum),
+           correct:null
+          }
         );
     }
   },
@@ -98,12 +136,42 @@ var Game = React.createClass({
     
     selectedNums.splice(index, 1);
     
-    this.setState({selectedNums: selectedNums});
+    this.setState({selectedNums: selectedNums,
+                   correct:null
+    });
+  },
+  
+  sumOfSelected: function() {
+    return (
+      this.state.selectedNums.reduce(function(a,b){
+        return a+b;
+      }, 0)
+    )
+  },
+  
+  checkAnswer: function() {
+    if(this.state.numStars === this.sumOfSelected()) {
+      this.setState({correct: true});
+    }
+    else {
+      this.setState({correct: false});
+    }
+  },
+  
+  acceptAnswer: function() {
+    var usedNums = this.state.usedNums.concat(this.state.selectedNums);
+    this.setState({usedNums:usedNums,
+                   concate:null,
+                   selectedNums:[],
+                   numStars:Math.floor(Math.random()*9) + 1
+    });
   },
   
   render: function() {
     var selectedNums = this.state.selectedNums,
-        numStars = this.state.numStars;
+        numStars = this.state.numStars,
+        correct = this.state.correct,
+        usedNums = this.state.usedNums;
     return (
       <div id="game">
         <h2>Play 9</h2>
@@ -111,14 +179,19 @@ var Game = React.createClass({
           <div id="clearfix">
           
           <StarsFrame numStars = {numStars} />
-          <ButtonFrame />
+          <ButtonFrame selectedNums={selectedNums} 
+                       correct={correct}
+                       checkAnswer={this.checkAnswer}
+                       acceptAnswer={this.acceptAnswer}/>
           <AnswerFrame selectedNums={selectedNums}
                        unclickNum={this.unclickNum} />
           
           </div>
           
           <NumbersFrame selectedNums={selectedNums}
-                        clickNum    ={this.clickNum}              />
+                        clickNum    ={this.clickNum} 
+                        usedNums    ={usedNums}
+                        acceptAnswer={this.acceptAnswer}/>
           
       </div>
     )
